118c4a7750da2b01ca8f77ae5d9af93711820291
AdsunRD <btduoc@adsun.vn>
2026-08-29 08:32:48 +0700

    MigrationFix

---
+++ DoctorAppointmentApi/DoctorAppointmentApi/Migrations/20260829012614_AdminUser_Fixed.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorAppointmentApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminUser_Fixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
